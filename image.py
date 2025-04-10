12e3ecedf14ffdf041bb3bc5f1d146bcea935011
student17 <student17@teb.pl>
2025-04-10 15:49:34 +0200

    comiit

---
+++ image.py
@@ -0,0 +1,4 @@
+class Image:
+    def __init__(self, path):
+        self.path = path
+        self.likes = 0
--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import sys
 
 from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QDialog, QApplication
+
+from image import Image
 from layout import Ui_Dialog
 
 
@@ -13,7 +15,9 @@ class MyForm(QDialog):
         self.ui.image.setPixmap(QPixmap('./pies.jpg'))
         self.ui.pushButton.clicked.connect(self.prev)
         self.ui.pushButton_2.clicked.connect(self.next)
-        self.images = ['pies.jpg', 'pies2.jpg', 'pies3.jpg']
+        self.ui.like_button.clicked.connect(self.like)
+        self.images = [Image('pies.jpg'), Image('pies2.jpg'), Image('pies3.jpg')]
+        # self.images = ['pies.jpg', 'pies2.jpg', 'pies3.jpg']
         self.index = 0
         self.show()
 
@@ -21,14 +25,19 @@ class MyForm(QDialog):
         self.index -= 1
         if self.index == -1:
             self.index = len(self.images) - 1
-        self.ui.image.setPixmap(QPixmap(self.images[self.index]))
+        self.ui.image.setPixmap(QPixmap(self.images[self.index].path))
+        self.ui.like_counter.setText(str(self.images[self.index].likes))
 
     def next(self):
         self.index += 1
         if self.index == len(self.images):
             self.index = 0
-        self.ui.image.setPixmap(QPixmap(self.images[self.index]))
+        self.ui.image.setPixmap(QPixmap(self.images[self.index].path))
+        self.ui.like_counter.setText(str(self.images[self.index].likes))
 
+    def like(self):
+        self.images[self.index].likes += 1
+        self.ui.like_counter.setText(str(self.images[self.index].likes))
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
